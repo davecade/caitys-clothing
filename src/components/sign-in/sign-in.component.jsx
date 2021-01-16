48c747750e2af05bba8b0ecd1834d8d48a1d2204
Dave <dave.cadelina@outlook.com>
2021-01-17 01:57:28 +1100

    added sign in with google API from firebase

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
+import { signInWithGoogle } from '../../firebase/firebase.utils'
 
 class SignIn extends Component {
     constructor(props) {
@@ -50,7 +51,10 @@ class SignIn extends Component {
                         required
                     />
 
-                    <CustomButton type="submit"> Sign In </CustomButton> 
+                    <CustomButton type="submit"> Sign In </CustomButton>
+                    <CustomButton onClick={signInWithGoogle}> 
+                        Sign In with Google
+                    </CustomButton>
                 </form>
             </div>
         )
