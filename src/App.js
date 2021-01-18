d4421878181b8c34c08c1353e7e2f5833ba31f46
Dave <dave.cadelina@outlook.com>
2021-01-19 00:18:31 +1100

    added auth on app.js file and converted to state class functi

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
 import './App.css';
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import HomePage from './pages/homepage/homepage.component'
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './shop/shop.component'
 import Header from './components/header/header.component'
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+import { auth } from './firebase/firebase.utils';
 
-const HatsPage = () => (
-    <div>
-      <h1>HATS PAGE</h1>
-    </div>
-)
 
-function App() {
-  return (
-      <Fragment>
-        <Header />
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUp} />
-        </Switch>
-      </Fragment>
-  )
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMont() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => this.setState({ currentUser: user }))
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+  
+  render() {
+
+      return (
+        <Fragment>
+          <Header />
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route path='/shop' component={ShopPage} />
+            <Route path='/signin' component={SignInAndSignUp} />
+          </Switch>
+        </Fragment>
+    )
+  }
 }
 
 export default App;
