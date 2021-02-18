4188e560e22091a5398c035159a3623d7be732e3
Dave <dave.cadelina@outlook.com>
2021-02-18 23:16:18 +1100

    added selectCartItemsCount selector

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -7,3 +7,10 @@ const selectCart = state => state.cart
 // -- this is now a memoize selector
 export const selectCartItems = createSelector([selectCart], cart => cart.cartItems)
 
+export const selectCartItemsCount = createSelector(
+    [selectCartItems],
+    cartItems => cartItems.reduce(
+        (accumulatedQuantity, cartItem) => (
+            accumulatedQuantity + cartItem.quantity
+            ), 0))
+
