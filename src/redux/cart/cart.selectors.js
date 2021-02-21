a036f218fc76971eb6f084740c252c904d17d614
Dave <dave.cadelina@outlook.com>
2021-02-21 23:39:13 +1100

    added reselect to all redux components and also refactored code using screateStructuredSelector

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -5,12 +5,19 @@ const selectCart = state => state.cart
 
 // -- Since we used createSelector to create this "selectCartItems" variable
 // -- this is now a memoize selector
-export const selectCartItems = createSelector([selectCart], cart => cart.cartItems)
+export const selectCartItems = createSelector(
+    [selectCart], cart => cart.cartItems
+)
+
+export const selectCartHidden = createSelector(
+    selectCart, cart => cart.cartHidden
+)
 
 export const selectCartItemsCount = createSelector(
     [selectCartItems],
     cartItems => cartItems.reduce(
         (accumulatedQuantity, cartItem) => (
             accumulatedQuantity + cartItem.quantity
-            ), 0))
+        ), 0)
+)
 
