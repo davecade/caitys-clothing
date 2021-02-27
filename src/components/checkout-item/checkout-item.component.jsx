712184af6c04d29ac35f96ecb6180f655e804a01
Dave <dave.cadelina@outlook.com>
2021-02-27 21:02:25 +1100

    destructured cartItems properly so we can still use the actual prop names

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -3,17 +3,23 @@ import './checkout-item.styles.scss'
 import { removeItem } from '../../redux/cart/cart.actions'
 import { connect } from 'react-redux'
 
-const CheckoutItem = ({ cartItem, removeItem}) => (
-    <div className='checkout-item'>
-        <div className="image-container">
-            <img src={cartItem.imageUrl} alt='item' />
+const CheckoutItem = ({ cartItem, removeItem}) => {
+
+    const { name, imageUrl, price, quantity } = cartItem;
+
+    return (
+        <div className='checkout-item'>
+            <div className="image-container">
+                <img src={imageUrl} alt='item' />
+            </div>
+            <span className='name'>{name}</span>
+            <span className='quantity'>{quantity}</span>
+            <span className='price'>{price}</span>
+            <div className='remove-button' onClick={() => removeItem(cartItem)}>&#10005;</div>
         </div>
-        <span className='name'>{cartItem.name}</span>
-        <span className='quantity'>{cartItem.quantity}</span>
-        <span className='price'>{cartItem.price}</span>
-        <div className='remove-button' onClick={() => removeItem(cartItem)}>&#10005;</div>
-    </div>
-)
+    )
+
+}
 
 const mapDispatchToProps = dispatch => ({
     removeItem: item => dispatch(removeItem(item))
