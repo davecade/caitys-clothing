9bf8b4e4e150bc4c173a3ecea93291cc9c1a54f3
Dave <dave.cadelina@outlook.com>
2021-03-03 21:09:39 +1100

    changed category name to collection

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './collection.styles.scss'
+import CollectionItem from '../../components/collection-item/collection-item.component'
+
+const CollectionPage = ({ match }) => {
+    console.log(match)
+    return (
+        <div className="collection-page">
+            <h2>COLLECTION PAGE</h2>
+        </div>
+    )
+}
+export default CollectionPage;
