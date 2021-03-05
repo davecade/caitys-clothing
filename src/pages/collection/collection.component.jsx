aaddf737be8b89065f245d05525de9b7b74351fb
Dave <dave.cadelina@outlook.com>
2021-03-05 23:26:48 +1100

    mapped through the items from the collection prop, and passed it CollectionItem to be rendered on screen with only the selected item type

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -5,10 +5,17 @@ import { connect } from 'react-redux'
 import { selectCollection } from '../../redux/shop/shop.selector'
 
 const CollectionPage = ({ collection }) => {
-    console.log(collection)
+    const { title, items }  = collection;
     return (
         <div className="collection-page">
-            <h2>COLLECTION PAGE</h2>
+            <h2 className="title">{title}</h2>
+            <div className='items'>
+                {
+                    items.map(item => (
+                        <CollectionItem key={item.id} item={item}/>
+                    ))
+                }
+            </div>
         </div>
         
     )
