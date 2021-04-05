d59ff6280c8414b87a1f43f8d2e783b7682e9a43
Dave <dave.cadelina@outlook.com>
2021-04-05 21:02:17 +1000

    Stripe fully integrated

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -5,9 +5,25 @@ const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price*100;
     const publishableKey = 'pk_test_51IciKZBsvFK7V5QEt63VqBlaUHkYNomzLhqKo7eIpzwdDhvvhad3WfpkTYMLv4GHugVzmqFRmt68e6au7trFe6JK00MK3BpK5E'
 
+    const onToken = token => {
+        console.log(token)
+        alert('Payment Successful')
+    }
+
     return(
-        <StrpeCheckout 
+        <StripeCheckout 
             label='Pay Now'
+            name="Crown Clothing Pty Ltd"
+            billingAddress
+            shippingAddress
+            image="https://svgshare.com/i/CUz.svg"
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
         />
     )
 }
+
+export default StripeCheckoutButton;
