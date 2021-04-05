a8d6a08b6a8e8f1c9228d901814657fd76723536
Dave <dave.cadelina@outlook.com>
2021-04-05 17:02:27 +1000

    added stripe-button component

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import StripeCheckout from 'react-stripe-checkout'
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price*100;
+    const publishableKey = 'pk_test_51IciKZBsvFK7V5QEt63VqBlaUHkYNomzLhqKo7eIpzwdDhvvhad3WfpkTYMLv4GHugVzmqFRmt68e6au7trFe6JK00MK3BpK5E'
+
+    return(
+        <StrpeCheckout 
+            label='Pay Now'
+        />
+    )
+}
