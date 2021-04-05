a3a5dc6dbbf5b34b955c5dd4aff1299781c2b335
Dave <dave.cadelina@outlook.com>
2021-04-05 21:18:15 +1000

    added favicon

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -35,6 +35,12 @@ const CheckoutPage = ({ cartItems, total }) => (
             <span>TOTAL: ${total}</span>
         </div>
 
+        <div className="test-warning">
+            *Please use the following test credit card for payments*
+            <br />
+            Card: "4242 4242 4242 4242" - Exp: "12/21" (or any future date) - CVV: "123"
+        </div>
+
         <StripeCheckoutButton price={total} />
     </div>
 )
