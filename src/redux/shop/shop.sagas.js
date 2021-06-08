6cb9bd1ea18ea7824a935802e9e00aabfb0a4bf6
Dave <dave.cadelina@outlook.com>
2021-06-08 22:19:55 +1000

    starting to transfer THUNK into Saga

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -2,8 +2,18 @@ import { takeEvery } from 'redux-saga/effects'
 
 import ShopActionTypes from './shop.types'
 
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils'
+import { fetchCollectionsSuccess,  fetchCollectionsFailure } from './shop.actions'
+
 export function* fetchCollectionsAsync() {
-    yield console.log("I am fired")
+    yield console.log("i am fired")
+    const collectionRef = firestore.collection('collections')
+
+        // collectionRef.get().then(snapshot => {
+        //     const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
+        //     dispatch(fetchCollectionsSuccess(collectionsMap))
+            
+        // }).catch(error => dispatch(fetchCollectionsFailure(error.message)))
 }
 
 export function* fetchCollectionsStart() {
