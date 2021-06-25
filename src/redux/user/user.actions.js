1f17d1a6e327f64bb0295e7fd9d9522467d3ae29
Dave <dave.cadelina@outlook.com>
2021-06-26 03:07:50 +1000

    added google sign in actions

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -6,3 +6,17 @@ export const setCurrentUser = user => {
         payload: user
     })
 }
+
+export const googleSignInStart = () => ({
+    type: UserActionTypes.GOOGLE_SIGN_IN_START
+})
+
+export const googleSignInSuccess = user => ({
+    type: UserActionTypes.GOOGLE_SIGN_IN_SUCCESS,
+    payload: user
+})
+
+export const googleSignInFailure = error => ({
+    type: UserActionTypes.GOOGLE_SIGN_IN_FAILURE,
+    payload: error
+})
