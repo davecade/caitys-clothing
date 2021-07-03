e6880c2177ce9f7abfde94aa9b298f9620185a77
Dave <dave.cadelina@outlook.com>
2021-07-03 22:55:09 +1000

    refactored signInStart function. Separated some repeated code into its own saga function

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -10,13 +10,22 @@ import {
     signInFailure,
 } from "./user.actions";
 
+export function* getSnapshotFromUserAuth(userAuth) {
+    try {
+        const userRef = yield call(createUserProfileDocument, userAuth)
+        const userSnapshot = yield userRef.get()
+        yield put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }))
+
+    } catch(error) {
+        yield put(signInFailure(error))
+    }
+}
+
 export function* signInWithGoogle() {
     try {
 
         const { user } = yield auth.signInWithPopup(googleProvider)
-        const userRef = yield call(createUserProfileDocument, user)
-        const userSnapshot = yield userRef.get()
-        yield put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }))
+        yield getSnapshotFromUserAuth(user)
 
     } catch(error) {
 
@@ -33,9 +42,7 @@ export function* onGoogleSignInStart() {
 export function* signInWithEmail({payload: {email, password}}) {
     try {
         const { user } = yield auth.signInWithEmailAndPassword(email, password)
-        const userRef = yield call(createUserProfileDocument, user)
-        const userSnapshot = yield userRef.get()
-        yield put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }))
+        yield getSnapshotFromUserAuth(user)
 
     } catch(error) {
         put(signInFailure(error))
