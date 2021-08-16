6ecfae88096dd6e1fe9bac8fcbb695c788c1c287
Dave <dave.cadelina@outlook.com>
2021-08-16 12:13:38 +1000

    added new spinner component and added it as the fallback for app.js for eacy lazy

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import './App.scss';
-import React, { useEffect, Fragment } from 'react';
-import HomePage from './pages/homepage/homepage.component'
+import React, { useEffect, Fragment, lazy, Suspense } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import ShopPage from './pages/shop/shop.component'
-import contactPage from './pages/contact/contact.component'
-import Submited from './pages/submitted/submited.component'
+//import HomePage from './pages/homepage/homepage.component'
+// import ShopPage from './pages/shop/shop.component'
+// import contactPage from './pages/contact/contact.component'
+// import Submited from './pages/submitted/submited.component'
 import Header from './components/header/header.component'
-import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
-import CheckoutPage from './pages/checkout/checkout.component'
+// import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+// import CheckoutPage from './pages/checkout/checkout.component'
 import { connect } from 'react-redux';
 import { selectCurrentUser } from './redux/user/user.selectors'
 import { createStructuredSelector } from 'reselect';
 import { selectShopDataForPreview } from './redux/shop/shop.selector';
 import { checkUserSession } from './redux/user/user.actions'
+import Spinner from './components/spinner/spinner.component'
 
+const HomePage = lazy (() => import('./pages/homepage/homepage.component'))
+const ShopPage = lazy (() => import('./pages/shop/shop.component'))
+const contactPage = lazy (() => import('./pages/contact/contact.component'))
+const Submited = lazy (() => import('./pages/submitted/submited.component'))
+//const Header = lazy (() => import('./components/header/header.component'))
+const SignInAndSignUp = lazy (() => import('./pages/sign-in-and-sign-up/sign-in-and-sign-up.component'))
+const CheckoutPage = lazy (() => import('./pages/checkout/checkout.component'))
 
 
 // -- Main App
@@ -27,12 +35,14 @@ const App = ({ checkUserSession, currentUser }) => {
     <Fragment>
       <Header />
       <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/contact' component={contactPage} />
-        <Route path='/submitted' component={Submited} />
-        <Route exact path='/checkout' component={CheckoutPage} />
-        <Route path='/signin' render={() => currentUser ? (<Redirect to="/"/>) : (<SignInAndSignUp />)}/>
+        <Suspense fallback={<Spinner />}>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/contact' component={contactPage} />
+          <Route path='/submitted' component={Submited} />
+          <Route exact path='/checkout' component={CheckoutPage} />
+          <Route path='/signin' render={() => currentUser ? (<Redirect to="/"/>) : (<SignInAndSignUp />)}/>
+        </Suspense>
       </Switch>
     </Fragment>
 )
